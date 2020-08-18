3620ec31984fbfd09689c433cfd7b726cf4b16fd
Omori Yasumitsu <yasumitsuoomori@gmail.com>
2020-08-18 19:17:54 +0900

    build: Add GitHub Actions

--- output.tf
+++ output.tf
@@ -5,3 +5,7 @@ output "github-access-key" {
 output "github-secret-access-key" {
   value = aws_iam_access_key.aws-ecr-github-access.secret
 }
+
+output "ecr-repository-url" {
+  value = aws_ecr_repository.test-repository.repository_url
+}
